bb2dc1623eb3be7d83e306475335e1bd3e846e08
Lamparo <LamparoS@Pavy.one>
2024-12-29 09:12:08 +0100

    --no-default-features

--- README.md
+++ README.md
@@ -8,15 +8,15 @@
 
 # Build code
 - `cargo +nightly watch -c -x fmt`
-- `cargo run --bin terrazzo-terminal --features server`
-- `cargo run --bin terrazzo-terminal --release --features server,max_level_info` to run it
-- `cargo build --bin terrazzo-terminal --release --features server,max_level_info && nohup ./target/release/terrazzo-terminal > /dev/null 2>&1 &` to run it in the background
+- `cargo run --bin terrazzo-terminal --no-default-features --features server`
+- `cargo run --bin terrazzo-terminal --release --no-default-features --features server,max_level_info` to run it
+- `cargo build --bin terrazzo-terminal --release --no-default-features --features server,max_level_info && nohup ./target/release/terrazzo-terminal > /dev/null 2>&1 &` to run it in the background
 
 # Clippy
-- `cargo clippy --bin terrazzo-terminal --features server,max_level_debug`
-- `cargo clippy --bin terrazzo-terminal --features server,max_level_info`
-- `cargo clippy --features client,max_level_debug`
-- `cargo clippy --features client,max_level_info`
+- `cargo clippy --bin terrazzo-terminal --no-default-features --features server,max_level_debug`
+- `cargo clippy --bin terrazzo-terminal --no-default-features --features server,max_level_info`
+- `cargo clippy --no-default-features --features client,max_level_debug`
+- `cargo clippy --no-default-features --features client,max_level_info`
 
 # Icons
 - https://icons.getbootstrap.com/
--- all.sh
+++ all.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-cargo test --features=client
-cargo test --features=server
-cargo test --features=client,server
-cargo clippy --features client,max_level_debug
-cargo clippy --features client,max_level_info
-cargo clippy --bin game --features server,max_level_debug
-cargo clippy --bin game --features server,max_level_info
-cargo clippy --bin terrazzo-terminal --features server,max_level_debug
-cargo clippy --bin terrazzo-terminal --features server,max_level_info
-cargo build --bin game --features server,max_level_debug
-cargo build --bin game --features server,max_level_info --release
-cargo build --bin terrazzo-terminal --features server,max_level_debug
-cargo build --bin terrazzo-terminal --features server,max_level_info --release
+cargo test --no-default-features --features client
+cargo test --no-default-features --features server
+cargo test --no-default-features --features client,server
+cargo clippy --no-default-features --features client,max_level_debug
+cargo clippy --no-default-features --features client,max_level_info
+cargo clippy --bin game --no-default-features --features server,max_level_debug
+cargo clippy --bin game --no-default-features --features server,max_level_info
+cargo clippy --bin terrazzo-terminal --no-default-features --features server,max_level_debug
+cargo clippy --bin terrazzo-terminal --no-default-features --features server,max_level_info
+cargo build --bin game --no-default-features --features server,max_level_debug
+cargo build --bin game --no-default-features --features server,max_level_info --release
+cargo build --bin terrazzo-terminal --no-default-features --features server,max_level_debug
+cargo build --bin terrazzo-terminal --no-default-features --features server,max_level_info --release
