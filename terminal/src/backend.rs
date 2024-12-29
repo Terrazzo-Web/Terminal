30004cf366edd59c63e42cd89500dcc3162d3d04
Lamparo <LamparoS@Pavy.one>
2024-12-29 17:19:21 +0100

    Start/Stop terrazzo terminal as a daemon process. (#4)
    * daemonize
    
    * Update backend.rs
    
    * CLI
    
    * Version
    
    * Use nix::libc

--- terminal/src/backend.rs
+++ terminal/src/backend.rs
@@ -1,8 +1,11 @@
 #![cfg(feature = "server")]
 
 use std::env::set_current_dir;
+use std::io::ErrorKind;
 use std::iter::once;
+use std::num::NonZeroI32;
 
+use clap::Parser as _;
 use terrazzo::axum;
 use terrazzo::axum::extract::Path;
 use terrazzo::axum::routing::get;
@@ -14,14 +17,58 @@ use tower_http::trace::TraceLayer;
 use tracing::enabled;
 use tracing::Level;
 
+use self::cli::Action;
+use self::cli::Cli;
 use crate::api;
 use crate::assets;
+
+mod cli;
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = if cfg!(debug_assertions) { 3000 } else { 3001 };
 
-pub async fn run_server() {
-    set_current_dir(std::env::var("HOME").expect("HOME")).expect("set_current_dir");
+pub fn run_server() -> std::io::Result<()> {
+    let cli = {
+        let mut cli = Cli::parse();
+        cli.pidfile = cli.pidfile.replace("$port", &cli.port.to_string());
+        cli
+    };
+
+    if cli.action == Action::Stop {
+        return cli.kill();
+    }
+
+    if let Some(pid) = cli.read_pid()? {
+        return Err(std::io::Error::new(
+            ErrorKind::AddrInUse,
+            format!("Already running PID = {pid}"),
+        ));
+    }
+
+    let address = format!("{}:{}", cli.host, cli.port);
+    println!("Listening on http://{address}");
+
+    match fork()? {
+        Some(_pid) => std::process::exit(0),
+        None => { /* in the child process */ }
+    }
+    check_err(unsafe { nix::libc::setsid() }, |r| r != 1)?;
+
+    match fork()? {
+        Some(pid) => {
+            println!("Server running in the background. PID = {pid}");
+            cli.save_pidfile(pid)?;
+            std::process::exit(0);
+        }
+        None => { /* in the child process */ }
+    }
+
+    run_server_async(&address)
+}
+
+#[tokio::main]
+async fn run_server_async(address: &str) -> std::io::Result<()> {
+    set_current_dir(std::env::var("HOME").expect("HOME"))?;
 
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::TRACE)
@@ -45,9 +92,20 @@ pub async fn run_server() {
     } else {
         router
     };
-    let listener = tokio::net::TcpListener::bind(format!("{HOST}:{PORT}"))
-        .await
-        .unwrap();
-    println!("Listening on http://{}", listener.local_addr().unwrap());
-    axum::serve(listener, router).await.unwrap();
+
+    let listener = tokio::net::TcpListener::bind(address).await?;
+    axum::serve(listener, router).await
+}
+
+fn fork() -> std::io::Result<Option<NonZeroI32>> {
+    let pid = check_err(unsafe { nix::libc::fork() }, |pid| pid != -1)?;
+    return Ok(NonZeroI32::new(pid));
+}
+
+fn check_err<IsOk: FnOnce(R) -> bool, R: Copy>(result: R, is_ok: IsOk) -> std::io::Result<R> {
+    if is_ok(result) {
+        Ok(result)
+    } else {
+        Err(std::io::Error::last_os_error())
+    }
 }
--- terminal/src/server.rs
+++ terminal/src/server.rs
@@ -1,6 +1,5 @@
 #![allow(unused_crate_dependencies)]
 
-#[tokio::main]
-async fn main() {
-    terrazzo_terminal::run_server().await
+fn main() -> std::io::Result<()> {
+    terrazzo_terminal::run_server()
 }
