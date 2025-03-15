01c077dbe7a50461efadcc47ea2572ab19086ead
Lamparo <LamparoS@Pavy.one>
2025-03-15 21:54:06 +0100

    memoize

--- terminal/src/backend.rs
+++ terminal/src/backend.rs
@@ -1,6 +1,7 @@
 #![cfg(feature = "server")]
 
 use std::env::set_current_dir;
+use std::sync::Arc;
 
 use clap::Parser as _;
 use cli::kill::KillServerError;
@@ -15,6 +16,8 @@ use tokio::signal::unix::signal;
 use trz_gateway_common::handle::ServerStopError;
 use trz_gateway_server::server::GatewayError;
 use trz_gateway_server::server::Server;
+use trz_gateway_server::server::gateway_config::GatewayConfig;
+use trz_gateway_server::server::gateway_config::memoize::MemoizedGatewayConfig;
 
 use self::cli::Action;
 use self::cli::Cli;
@@ -42,24 +45,27 @@ pub fn run_server() -> Result<(), RunServerError> {
         return Ok(cli.kill()?);
     }
 
-    let root_ca = PrivateRootCa::load(&cli)?;
-    let tls_config = make_tls_config(&root_ca)?;
-    let config = TerminalBackendServer {
-        host: cli.host.clone(),
-        port: cli.port,
-        root_ca,
-        tls_config,
-    };
+    let config = MemoizedGatewayConfig::new(true, || {
+        let root_ca = PrivateRootCa::load(&cli)?;
+        let tls_config = make_tls_config(&root_ca)?;
+        let config = TerminalBackendServer {
+            host: cli.host.clone(),
+            port: cli.port,
+            root_ca,
+            tls_config,
+        };
 
-    if cli.action == Action::Start {
-        self::daemonize::daemonize(cli)?;
-    }
+        if cli.action == Action::Start {
+            self::daemonize::daemonize(cli)?;
+        }
+        Ok::<RunServerError>(Arc::new(config))
+    });
 
     return run_server_async(config);
 }
 
 #[tokio::main]
-async fn run_server_async(config: TerminalBackendServer) -> Result<(), RunServerError> {
+async fn run_server_async(config: impl GatewayConfig) -> Result<(), RunServerError> {
     set_current_dir(std::env::var("HOME").expect("HOME")).map_err(RunServerError::SetCurrentDir)?;
 
     assets::install_assets();
