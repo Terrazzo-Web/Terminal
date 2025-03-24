1735818278d3c11ad5eb6a8b493ac44d2b58360c
Lamparo <LamparoS@Pavy.one>
2025-03-24 22:19:54 +0100

    Revert "ReleaseOnDrop pinnned"
    This reverts commit 18fe0ba92210f828967e17a1179f0a72a7e9a315.

--- pty/src/release_on_drop.rs
+++ pty/src/release_on_drop.rs
@@ -1,29 +1,9 @@
-use std::pin::Pin;
-
 use futures::channel::oneshot;
-use pin_project::pin_project;
-use pin_project::pinned_drop;
 use tracing::warn;
 
-#[pin_project(PinnedDrop, project = ReleaseOnDropProj)]
 pub struct ReleaseOnDrop<T> {
-    #[pin]
-    value: OptionPinned<T>,
+    value: Option<T>,
     on_drop: Option<oneshot::Sender<T>>,
-}
-
-#[pin_project(project = OptionPinnedProj)]
-#[derive(Default)]
-enum OptionPinned<T> {
-    Some(#[pin] T),
-    #[default]
-    None,
-}
-
-impl<T> Default for OptionPinnedProj<'_, T> {
-    fn default() -> Self {
-        Self::None
-    }
 }
 
 impl<T> ReleaseOnDrop<T> {
@@ -31,7 +11,7 @@ impl<T> ReleaseOnDrop<T> {
         let (tx, rx) = oneshot::channel();
         (
             Self {
-                value: OptionPinned::Some(value),
+                value: Some(value),
                 on_drop: Some(tx),
             },
             rx,
@@ -39,42 +19,24 @@ impl<T> ReleaseOnDrop<T> {
     }
 }
 
-impl<T> AsRef<T> for ReleaseOnDrop<T> {
-    fn as_ref(&self) -> &T {
-        let OptionPinned::Some(value) = &self.value else {
-            unreachable!()
-        };
-        return value;
-    }
-}
-
 impl<T> AsMut<T> for ReleaseOnDrop<T> {
     fn as_mut(&mut self) -> &mut T {
-        let OptionPinned::Some(value) = &mut self.value else {
-            unreachable!()
-        };
-        return value;
+        let value = self.value.as_mut();
+        unsafe { value.unwrap_unchecked() }
     }
 }
 
-impl<T> ReleaseOnDropProj<'_, T> {
-    pub fn as_pin(&mut self) -> Pin<&mut T> {
-        let OptionPinnedProj::Some(value) = self.value.as_mut().project() else {
-            unreachable!()
-        };
-        return value;
+impl<T> AsRef<T> for ReleaseOnDrop<T> {
+    fn as_ref(&self) -> &T {
+        let value = self.value.as_ref();
+        unsafe { value.unwrap_unchecked() }
     }
 }
 
-#[pinned_drop]
-impl<T> PinnedDrop for ReleaseOnDrop<T> {
-    fn drop(self: Pin<&mut Self>) {
-        let this = self.project();
-        let value = std::mem::take(&mut this.value.project());
-        let OptionPinnedProj::Some(value) = value else {
-            panic!("ReleaseOnDrop: double drop?");
-        };
-        if let Some(on_drop) = this.on_drop.take() {
+impl<T> Drop for ReleaseOnDrop<T> {
+    fn drop(&mut self) {
+        let value = self.value.take().expect("ReleaseOnDrop: double drop?");
+        if let Some(on_drop) = self.on_drop.take() {
             let result = on_drop.send(value);
             if cfg!(debug_assertions) && result.is_err() {
                 warn!("ReleaseOnDrop: Unable to release on drop");
