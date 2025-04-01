2890e4d84d60a6b030c33928dfd7c131b1ac8f6a
Lamparo <LamparoS@Pavy.one>
2025-04-01 17:15:57 +0200

    to_json_stream

--- terminal/src/api/client/channel/json_stream.rs
+++ terminal/src/api/client/channel/json_stream.rs
@@ -1,48 +1,75 @@
+use std::future::Ready;
 use std::future::ready;
 
 use futures::Stream;
-use futures::StreamExt;
+use futures::StreamExt as _;
+use futures::future;
+use futures::stream::once;
 use nameth::NamedEnumValues as _;
 use nameth::nameth;
 use wasm_bindgen::JsCast as _;
 use wasm_bindgen::JsValue;
 use web_sys::js_sys::Uint8Array;
 
-pub fn to_json_stream<O: 'static>(
+use crate::api::NEWLINE;
+
+pub fn to_json_stream<O>(
     body: web_sys::ReadableStream,
-) -> impl Stream<Item = Result<O, JsonStreamError>> + 'static {
+) -> impl Stream<Item = Result<O, JsonStreamError>> {
     let stream = wasm_streams::ReadableStream::from_raw(body).into_stream();
     let stream = stream.scan(JsonStreamState::default(), |state, chunk| {
-        // let buffer = match state {
-        //     JsonStreamState::EOS => return None,
-        //     JsonStreamState::Buffer(buffer) => buffer,
-        // };
-
-        // let chunk = match chunk {
-        //     Ok(chunk) => chunk,
-        //     Err(error) => {
-        //         *state = JsonStreamState::EOS;
-        //         return Some(Err(JsonStreamError::Error(error)));
-        //     }
-        // };
-
-        // let Some(chunk) = chunk.dyn_ref::<Uint8Array>() else {
-        //     return Some(Err(JsonStreamError::BadChunk(chunk)));
-        // };
-
-        // let count = chunk.length() as usize;
-        // let old_len = buffer.len();
-        // let new_len = old_len + count;
-        // buffer.extend(std::iter::repeat(b'\0').take(count));
-        // let slice = &mut buffer[old_len..new_len];
-        // chunk.copy_to(slice);
-        return ready(Some(Err(JsonStreamError::Error(JsValue::null()))));
+        ready(process_chunks(state, chunk))
     });
-    check_type::<O>(&stream);
-    return stream;
+    return stream.flatten();
 }
 
-fn check_type<O>(_: &impl Stream<Item = Result<O, JsonStreamError>>) {}
+fn process_chunks<O>(
+    state: &mut JsonStreamState,
+    chunk: Result<JsValue, JsValue>,
+) -> Option<impl Stream<Item = Result<O, JsonStreamError>> + use<O>> {
+    let buffer = match state {
+        JsonStreamState::EOS => return None,
+        JsonStreamState::Buffer(buffer) => buffer,
+    };
+
+    let chunk = match chunk {
+        Ok(chunk) => chunk,
+        Err(error) => {
+            *state = JsonStreamState::EOS;
+            return Some(once(ready(Err(JsonStreamError::Error(error)))).left_stream());
+        }
+    };
+
+    let Some(chunk) = chunk.dyn_ref::<Uint8Array>() else {
+        return Some(once(ready(Err(JsonStreamError::BadChunk(chunk)))).left_stream());
+    };
+
+    let old_len = buffer.len();
+    let new_len = old_len + chunk.length() as usize;
+    buffer.resize(new_len, 0);
+    chunk.copy_to(&mut buffer[old_len..new_len]);
+
+    return Some(futures::stream::iter(process_chunk(buffer)).right_stream());
+}
+
+fn process_chunk<O>(
+    buffer: &mut Vec<u8>,
+) -> impl Iterator<Item = Result<O, JsonStreamError>> + use<O> {
+    let mut consumed = 0;
+    let mut objects = vec![];
+    for chunk in buffer.split_inclusive(|c| *c == NEWLINE) {
+        if chunk.last() == Some(&NEWLINE) {
+            consumed += chunk.len();
+            objects.push(parse_chunk(&chunk[..chunk.len() - 1]));
+        }
+    }
+    buffer.drain(..consumed);
+    return objects.into_iter();
+}
+
+fn parse_chunk<O>(_chunk: &[u8]) -> Result<O, JsonStreamError> {
+    todo!()
+}
 
 enum JsonStreamState {
     EOS,
