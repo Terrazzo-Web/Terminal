568ea444a1747769061807a08fd8e6d90c8a37e4
Lamparo <LamparoS@Pavy.one>
2025-05-11 08:46:34 +0200

    ack remotes

--- terminal/src/api/server/stream/ack.rs
+++ terminal/src/api/server/stream/ack.rs
@@ -1,11 +1,27 @@
 use std::sync::Arc;
 
 use terrazzo::axum::Json;
+use trz_gateway_common::http_error::HttpError;
 use trz_gateway_server::server::Server;
 
 use crate::api::AckRequest;
-use crate::backend::throttling_stream;
+use crate::backend::client_service::ack;
+use crate::backend::client_service::ack::AckError;
+use crate::backend::protos::terrazzo::gateway::client::AckRequest as AckRequestProto;
 
-pub async fn ack(_server: Arc<Server>, Json(request): Json<AckRequest>) {
-    throttling_stream::ack(&request.terminal.id, request.ack)
+pub async fn ack(
+    server: Arc<Server>,
+    Json(request): Json<AckRequest>,
+) -> Result<(), HttpError<AckError>> {
+    let client_address = request.terminal.via.as_slice().to_vec();
+    let response = ack::ack(
+        &server,
+        &client_address,
+        AckRequestProto {
+            terminal: Some(request.terminal.into()),
+            ack: request.ack as u64,
+        },
+    )
+    .await;
+    Ok(response?)
 }
