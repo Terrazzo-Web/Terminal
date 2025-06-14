8214998a605e66ee8f34ec86144e451e163909a5
Lamparo <LamparoS@Pavy.one>
2025-06-14 13:44:25 +0200

    SideView add/remove OK

--- terminal/src/text_editor/side.rs
+++ terminal/src/text_editor/side.rs
@@ -3,6 +3,8 @@
 use std::collections::BTreeMap;
 use std::sync::Arc;
 
+use nameth::NamedEnumValues as _;
+use nameth::nameth;
 use tracing::debug;
 use tracing::warn;
 
@@ -83,6 +85,73 @@ fn add_file(
             Arc::new(into_children)
         }
     }
+}
+
+fn remove_file(
+    into_children: Arc<Children>,
+    relative_path: &[&Arc<str>],
+) -> Result<Arc<Children>, RemoveFileError> {
+    match relative_path {
+        [] => remove_file(into_children, &[&"/".into()]),
+        [child_name] => {
+            #[cfg(debug_assertions)]
+            match into_children.get(*child_name) {
+                Some(child) => match &**child {
+                    SideView::Folder { .. } => debug!("Remove folder {child_name}"),
+                    SideView::File { .. } => debug!("Remove file {child_name}"),
+                },
+                None => {
+                    debug!("The file wasn't here {child_name}");
+                    return Err(RemoveFileError::FileNotFound);
+                }
+            }
+            let mut into_children = (*into_children).clone();
+            into_children.remove(*child_name);
+            Ok(Arc::new(into_children))
+        }
+        [folder_name, rest @ ..] => {
+            let folder_name = *folder_name;
+            let children = match into_children.get(folder_name) {
+                Some(child) => match &**child {
+                    SideView::Folder { name: _, children } => {
+                        debug!("Removing from folder {folder_name}");
+                        children.clone()
+                    }
+                    SideView::File(expected_folder) => {
+                        return Err(RemoveFileError::ExpectedFolder(
+                            expected_folder.name.clone(),
+                        ));
+                    }
+                },
+                None => {
+                    return Err(RemoveFileError::ParentNotFound(folder_name.clone()));
+                }
+            };
+            let mut into_children = (*into_children).clone();
+            let new_children = remove_file(children, rest)?;
+            into_children.insert(
+                folder_name.clone(),
+                Arc::new(SideView::Folder {
+                    name: folder_name.clone(),
+                    children: new_children,
+                }),
+            );
+            Ok(Arc::new(into_children))
+        }
+    }
+}
+
+#[nameth]
+#[derive(thiserror::Error, Debug, serde::Serialize, serde::Deserialize)]
+pub enum RemoveFileError {
+    #[error("[{n}] File can't be a child of file {0}", n = self.name())]
+    ExpectedFolder(Arc<str>),
+
+    #[error("[{n}] Parent folder does not exist: {0}", n = self.name())]
+    ParentNotFound(Arc<str>),
+
+    #[error("[{n}] The file was not found", n = self.name())]
+    FileNotFound,
 }
 
 #[cfg(test)]
@@ -265,6 +334,159 @@ mod tests {
                 },
             },
         },
+    },
+}"#
+            .trim(),
+            format!("{tree:#?}")
+        );
+    }
+
+    #[test]
+    fn remove_file() {
+        let tree = Arc::<Children>::default();
+        let make_file = |name: &str| {
+            Arc::new(FileMetadata {
+                name: Arc::from(name),
+                size: Some(12),
+                is_dir: false,
+                created: None,
+                accessed: None,
+                modified: None,
+                mode: None,
+                user: None,
+                group: None,
+            })
+        };
+        let tree = super::add_file(
+            tree,
+            &[&Arc::from("a1"), &Arc::from("b1"), &Arc::from("c1.txt")],
+            make_file("c1.txt"),
+        );
+        assert_eq!(
+            r#"
+{
+    "a1": Folder {
+        name: "a1",
+        children: {
+            "b1": Folder {
+                name: "b1",
+                children: {
+                    "c1.txt": File(
+                        "c1.txt",
+                    ),
+                },
+            },
+        },
+    },
+}"#
+            .trim(),
+            format!("{tree:#?}")
+        );
+
+        let tree = super::add_file(
+            tree,
+            &[&Arc::from("a1"), &Arc::from("b1"), &Arc::from("c2.txt")],
+            make_file("c2.txt"),
+        );
+        assert_eq!(
+            r#"
+{
+    "a1": Folder {
+        name: "a1",
+        children: {
+            "b1": Folder {
+                name: "b1",
+                children: {
+                    "c1.txt": File(
+                        "c1.txt",
+                    ),
+                    "c2.txt": File(
+                        "c2.txt",
+                    ),
+                },
+            },
+        },
+    },
+}"#
+            .trim(),
+            format!("{tree:#?}")
+        );
+
+        // Remove file: ExpectedFolder
+        let error = super::remove_file(
+            tree.clone(),
+            &[
+                &Arc::from("a1"),
+                &Arc::from("b1"),
+                &Arc::from("c2.txt"),
+                &Arc::from("not_found.txt"),
+            ],
+        )
+        .unwrap_err();
+        assert_eq!(
+            "[ExpectedFolder] File can't be a child of file c2.txt",
+            format!("{error}")
+        );
+
+        // Remove file: ParentNotFound
+        let error = super::remove_file(
+            tree.clone(),
+            &[
+                &Arc::from("a1"),
+                &Arc::from("b1"),
+                &Arc::from("c3.txt"),
+                &Arc::from("not_found.txt"),
+            ],
+        )
+        .unwrap_err();
+        assert_eq!(
+            "[ParentNotFound] Parent folder does not exist: c3.txt",
+            format!("{error}")
+        );
+
+        // Remove file: FileNotFound
+        let error = super::remove_file(
+            tree.clone(),
+            &[&Arc::from("a1"), &Arc::from("b1"), &Arc::from("c3.txt")],
+        )
+        .unwrap_err();
+        assert_eq!("[FileNotFound] The file was not found", format!("{error}"));
+
+        // Remove file
+        let tree = super::remove_file(
+            tree,
+            &[&Arc::from("a1"), &Arc::from("b1"), &Arc::from("c2.txt")],
+        )
+        .unwrap();
+        assert_eq!(
+            r#"
+{
+    "a1": Folder {
+        name: "a1",
+        children: {
+            "b1": Folder {
+                name: "b1",
+                children: {
+                    "c1.txt": File(
+                        "c1.txt",
+                    ),
+                },
+            },
+        },
+    },
+}"#
+            .trim(),
+            format!("{tree:#?}")
+        );
+
+        // Remove folder
+        let tree = super::remove_file(tree, &[&Arc::from("a1"), &Arc::from("b1")]).unwrap();
+        assert_eq!(
+            r#"
+{
+    "a1": Folder {
+        name: "a1",
+        children: {},
     },
 }"#
             .trim(),
