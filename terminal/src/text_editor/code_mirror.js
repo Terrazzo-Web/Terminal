66151f623f7d56febba9a022ec6e4394b4a62793
Lamparo <LamparoS@Pavy.one>
2025-06-19 12:03:47 +0200

    Use npm for XtermJS (#50)

--- terminal/src/text_editor/code_mirror.js
+++ terminal/src/text_editor/code_mirror.js
@@ -1,19 +1,19 @@
 class CodeMirrorJs {
     editorView;
     constructor(element, content, onchange) {
-        const updateListener = CodeMirror.EditorView.updateListener.of((update) => {
+        const updateListener = JsDeps.EditorView.updateListener.of((update) => {
             if (update.docChanged) {
                 const content = update.state.doc.toString();
                 onchange(content);
             }
         });
 
-        const state = CodeMirror.EditorState.create({
+        const state = JsDeps.EditorState.create({
             doc: content,
-            extensions: [CodeMirror.basicSetup, updateListener]
+            extensions: [JsDeps.basicSetup, updateListener]
         });
 
-        this.editorView = new CodeMirror.EditorView({
+        this.editorView = new JsDeps.EditorView({
             state,
             parent: element,
         });
