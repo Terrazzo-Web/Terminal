72f025dc064fa043b9b77db8e0c8284d5aee1cf0
Lamparo <LamparoS@Pavy.one>
2025-07-12 20:01:56 +0200

    mod

--- terminal/src/terminal/mod.rs
+++ terminal/src/terminal/mod.rs
@@ -18,7 +18,7 @@ use self::terminal_tabs::TerminalTabs;
 use crate::api::client::terminal_api;
 use crate::terminal_id::TerminalId;
 
-stylance::import_crate_style!(style, "src/terminal.scss");
+stylance::import_crate_style!(style, "src/terminal/terminal.scss");
 
 mod attach;
 mod javascript;
