392298c6a8106b944b9b1d415621f6970a770592
Lamparo <LamparoS@Pavy.one>
2025-07-19 07:45:34 +0200

    print_oid

--- terminal/src/converter/service/pkcs7.rs
+++ terminal/src/converter/service/pkcs7.rs
@@ -1,9 +1,12 @@
+use std::sync::OnceLock;
+
 use base64::Engine as _;
 use base64::prelude::BASE64_STANDARD;
 use cms::cert::x509::der::Decode as _;
 use cms::cert::x509::der::Encode as _;
 use cms::cert::x509::der::Tagged as _;
 use oid_registry::OID_PKCS7_ID_SIGNED_DATA;
+use oid_registry::OidRegistry;
 use openssl::x509::X509;
 
 use super::AddConversionFn;
@@ -34,7 +37,7 @@ fn add_pkcs7_impl(input: &[u8], add: &mut impl AddConversionFn) -> Option<()> {
         .map(AlgorithmIdentifier::from)
         .collect();
     let encapsulated_content_info = EncapsulatedContentInfo {
-        encapsulated_content_type: encap_content_info.econtent_type.to_string(),
+        encapsulated_content_type: print_oid(encap_content_info.econtent_type),
         encapsulated_content: encap_content_info.econtent.map(Any::from),
     };
     let certificates = {
@@ -53,7 +56,7 @@ fn add_pkcs7_impl(input: &[u8], add: &mut impl AddConversionFn) -> Option<()> {
                     }
                     cms::cert::CertificateChoices::Other(other) => {
                         CertificateChoices::Other(OtherCertificateFormat {
-                            format: other.other_cert_format.to_string(),
+                            format: print_oid(other.other_cert_format),
                             certificate: other.other_cert.into(),
                         })
                     }
@@ -73,7 +76,7 @@ fn add_pkcs7_impl(input: &[u8], add: &mut impl AddConversionFn) -> Option<()> {
                         version: format!("{:?}", list.tbs_cert_list.version),
                         signature: list.tbs_cert_list.signature.into(),
                         issuer: list.tbs_cert_list.issuer.to_string(),
-                        this_update: list.tbs_cert_list.this_update.to_string(),
+                        this_update: list.tbs_cert_list.this_update.to_string(), //todo
                         next_update: list.tbs_cert_list.next_update.map(|n| n.to_string()),
                         revoked_certificates: make_revoked_certificates(
                             list.tbs_cert_list.revoked_certificates,
@@ -122,10 +125,7 @@ fn add_pkcs7_impl(input: &[u8], add: &mut impl AddConversionFn) -> Option<()> {
             },
             digest_algorithm: signer_info.digest_alg.into(),
             signed_attributes: make_attributes(signer_info.signed_attrs),
-            signature_algorithm: AlgorithmIdentifier {
-                oid: signer_info.signature_algorithm.oid.to_string(),
-                parameters: signer_info.signature_algorithm.parameters.map(Any::from),
-            },
+            signature_algorithm: signer_info.signature_algorithm.into(),
             signature: print_bytes(signer_info.signature.as_bytes()),
             unsigned_attributes: make_attributes(signer_info.unsigned_attrs),
         })
@@ -140,7 +140,7 @@ fn add_pkcs7_impl(input: &[u8], add: &mut impl AddConversionFn) -> Option<()> {
         signer_infos,
     };
     let content_info = ContentInfo {
-        content_type: content_info.content_type.to_string(),
+        content_type: print_oid(content_info.content_type),
         content: signed_data,
     };
     let content_info =
@@ -214,7 +214,7 @@ impl From<cms::cert::x509::spki::AlgorithmIdentifier<cms::cert::x509::der::Any>>
 {
     fn from(value: cms::cert::x509::spki::AlgorithmIdentifier<cms::cert::x509::der::Any>) -> Self {
         Self {
-            oid: value.oid.to_string(),
+            oid: print_oid(value.oid),
             parameters: value.parameters.map(Any::from),
         }
     }
@@ -279,7 +279,7 @@ struct Extension {
 impl From<cms::cert::x509::ext::Extension> for Extension {
     fn from(extension: cms::cert::x509::ext::Extension) -> Self {
         Self {
-            extn_id: extension.extn_id.to_string(),
+            extn_id: print_oid(extension.extn_id),
             critical: extension.critical,
             extn_value: BASE64_STANDARD.encode(extension.extn_value),
         }
@@ -325,12 +325,12 @@ fn make_attributes(
     attributes: Option<cms::cert::x509::der::asn1::SetOfVec<cms::cert::x509::attr::Attribute>>,
 ) -> Vec<Attribute> {
     attributes
-        .map(|unsigned_attrs| unsigned_attrs.into_vec())
+        .map(|attributes| attributes.into_vec())
         .unwrap_or_default()
         .into_iter()
-        .map(|unsigned_attr| Attribute {
-            oid: unsigned_attr.oid.to_string(),
-            values: unsigned_attr
+        .map(|attribute| Attribute {
+            oid: print_oid(attribute.oid),
+            values: attribute
                 .values
                 .into_vec()
                 .into_iter()
@@ -365,4 +365,13 @@ fn print_bytes(bytes: &[u8]) -> String {
         };
     }
     result
+}
+
+fn print_oid(oid: cms::cert::x509::spki::ObjectIdentifier) -> String {
+    static OID_REGISTRY: OnceLock<OidRegistry> = OnceLock::new();
+    let oid_registry = OID_REGISTRY.get_or_init(|| OidRegistry::default().with_all_crypto());
+    oid_registry
+        .get(&oid_registry::Oid::new(oid.as_bytes().into()))
+        .map(|oid| oid.description().to_owned())
+        .unwrap_or_else(|| oid.to_string())
 }
