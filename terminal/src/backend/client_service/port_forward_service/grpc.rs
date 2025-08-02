a02e076f272ecc9f0f5e474f0d5e06a4493335f6
Lamparo <LamparoS@Pavy.one>
2025-08-02 20:33:56 +0200

    wip Sink

--- terminal/src/backend/client_service/port_forward_service/grpc.rs
+++ terminal/src/backend/client_service/port_forward_service/grpc.rs
@@ -1,8 +1,3 @@
-use std::pin::Pin;
-use std::task::Context;
-use std::task::Poll;
-
-use futures::Stream;
 use tonic::Request;
 use tonic::Response;
 use tonic::Status;
@@ -11,8 +6,9 @@ use tonic::async_trait;
 
 use super::bind::BindStream;
 use crate::backend::client_service::ClientServiceImpl;
+use crate::backend::client_service::port_forward_service::download::DownloadStream;
+use crate::backend::client_service::port_forward_service::upload::UploadStream;
 use crate::backend::protos::terrazzo::portforward::PortForwardDataRequest;
-use crate::backend::protos::terrazzo::portforward::PortForwardDataResponse;
 use crate::backend::protos::terrazzo::portforward::PortForwardEndpoint;
 use crate::backend::protos::terrazzo::portforward::port_forward_service_server::PortForwardService;
 
@@ -24,44 +20,28 @@ impl PortForwardService for ClientServiceImpl {
         &self,
         requests: Request<Streaming<PortForwardEndpoint>>,
     ) -> Result<Response<BindStream>, Status> {
-        let stream = super::bind::dispatch(&self.server, requests.into_inner()).await?;
-        Ok(Response::new(stream))
+        let upload_stream = super::bind::dispatch(&self.server, requests.into_inner()).await?;
+        Ok(Response::new(upload_stream))
     }
 
     type DownloadStream = DownloadStream;
 
     async fn download(
         &self,
-        _request: Request<tonic::Streaming<PortForwardDataRequest>>,
-    ) -> Result<Response<Self::DownloadStream>, Status> {
-        todo!()
+        requests: Request<tonic::Streaming<PortForwardDataRequest>>,
+    ) -> Result<Response<DownloadStream>, Status> {
+        let download_stream =
+            super::download::download(&self.server, requests.into_inner()).await?;
+        Ok(Response::new(download_stream))
     }
 
     type UploadStream = UploadStream;
 
     async fn upload(
         &self,
-        _request: Request<tonic::Streaming<PortForwardDataRequest>>,
-    ) -> Result<Response<Self::UploadStream>, Status> {
-        todo!()
-    }
-}
-
-pub struct DownloadStream;
-pub struct UploadStream;
-
-impl Stream for DownloadStream {
-    type Item = Result<PortForwardDataResponse, Status>;
-
-    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        todo!()
-    }
-}
-
-impl Stream for UploadStream {
-    type Item = Result<PortForwardDataResponse, Status>;
-
-    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        todo!()
+        requests: Request<tonic::Streaming<PortForwardDataRequest>>,
+    ) -> Result<Response<UploadStream>, Status> {
+        let upload_stream = super::upload::upload(&self.server, requests.into_inner()).await?;
+        Ok(Response::new(upload_stream))
     }
 }
