b76ffd014329f12649493315a532202ce76bfc09
Lamparo <LamparoS@Pavy.one>
2025-08-02 22:30:40 +0200

    Upload

--- terminal/src/backend/client_service/port_forward_service/upload.rs
+++ terminal/src/backend/client_service/port_forward_service/upload.rs
@@ -5,6 +5,8 @@ use std::sync::Arc;
 use std::task::Context;
 use std::task::Poll;
 
+use futures::AsyncWriteExt as _;
+use futures::SinkExt as _;
 use futures::Stream;
 use futures::StreamExt as _;
 use futures::stream;
@@ -14,12 +16,12 @@ use nameth::nameth;
 use pin_project::pin_project;
 use prost::bytes::Bytes;
 use scopeguard::defer;
-use tokio::io::AsyncRead;
-use tokio::io::AsyncWriteExt as _;
+use tokio::io::AsyncRead as _;
+use tokio::io::AsyncWrite as _;
 use tokio::io::ReadBuf;
+use tokio::net::TcpStream;
 use tokio::net::tcp::OwnedReadHalf;
 use tokio::net::tcp::OwnedWriteHalf;
-use tokio::sync::oneshot;
 use tonic::Status;
 use tonic::Streaming;
 use tonic::body::Body as BoxBody;
@@ -34,7 +36,6 @@ use tracing::warn;
 use trz_gateway_server::server::Server;
 
 use super::RequestDataStream;
-use super::listeners::EndpointId;
 use crate::backend::client_service::routing::DistributedCallback;
 use crate::backend::client_service::routing::DistributedCallbackError;
 use crate::backend::protos::terrazzo::portforward::PortForwardDataRequest;
@@ -44,7 +45,7 @@ use crate::backend::protos::terrazzo::portforward::port_forward_data_request;
 use crate::backend::protos::terrazzo::portforward::port_forward_service_client::PortForwardServiceClient;
 use crate::backend::protos::terrazzo::shared::ClientAddress;
 
-/// Upload data to given endpoint
+/// Upload data from listener
 pub async fn upload(
     server: &Arc<Server>,
     mut download_stream: impl RequestDataStream,
@@ -60,9 +61,9 @@ pub async fn upload(
         debug!("Uploading data to: {endpoint:?}");
 
         let remote = endpoint.remote.clone().unwrap_or_default();
-        let download_stream =
+        let upload_stream =
             UploadCallback::process(server, &remote.via, (endpoint, download_stream)).await?;
-        return Ok(download_stream);
+        return Ok(upload_stream);
     };
     return task.instrument(info_span!("PortForward Upload")).await;
 }
@@ -151,9 +152,9 @@ impl<S: RequestDataStream> DistributedCallback for UploadCallback<S> {
             let download_stream = stream::once(ready(first_message))
                 .chain(download_stream.filter_map(|next| ready(next.ok())));
             let mut client = PortForwardServiceClient::new(channel);
-            let download_stream = client.upload(download_stream).await?;
+            let upload_stream = client.upload(download_stream).await?;
             Ok(UploadStream::Remote(RemoteUploadStream(
-                download_stream.into_inner(),
+                upload_stream.into_inner(),
             )))
         }
         .instrument(info_span!("Remote"))
@@ -168,32 +169,11 @@ impl<S: RequestDataStream> DistributedCallback for UploadCallback<S> {
             debug!("Start");
             defer!(debug!("End"));
 
-            let endpoint_id = EndpointId {
-                host: endpoint.host,
-                port: endpoint.port,
-            };
-
-            let (future_streams, tx) = {
-                let mut listeners = super::listeners::listeners();
-                let Some(future_streams) = listeners.get_mut(&endpoint_id) else {
-                    return Err(UploadLocalError::StreamsNotRegistered(endpoint_id));
-                };
-                let (tx, rx) = oneshot::channel();
-                (std::mem::replace(future_streams, rx), tx)
-            };
-            let (read_half, write_half) = {
-                let streams = future_streams
-                    .await
-                    .map_err(UploadLocalError::StreamsNotAvailable)?;
-                let mut streams = scopeguard::guard(streams, |streams| {
-                    let _ = tx.send(streams);
-                });
-                streams
-                    .recv()
-                    .await
-                    .ok_or(UploadLocalError::NoMoreStreams)?
-                    .into_split()
-            };
+            let PortForwardEndpoint { host, port, .. } = endpoint;
+            let (read_half, write_half) = TcpStream::connect(format!("{host}:{port}"))
+                .await
+                .map_err(UploadLocalError::Connect)?
+                .into_split();
 
             let requests_task = process_write_half(download_stream, write_half);
             tokio::spawn(requests_task.in_current_span());
@@ -209,44 +189,82 @@ impl<S: RequestDataStream> DistributedCallback for UploadCallback<S> {
 
 async fn process_write_half(
     mut download_stream: impl RequestDataStream,
-    mut write_half: OwnedWriteHalf,
+    write_half: OwnedWriteHalf,
 ) {
+    let mut sink = WriteHalf(write_half).into_sink::<Bytes>().buffer(8192);
     while let Some(next) = download_stream.next().await {
         match next {
             Ok(PortForwardDataRequest {
                 kind: Some(port_forward_data_request::Kind::Endpoint(endpoint)),
-            }) => return warn!("Invalid next message is endpoint: {endpoint:?}"),
+            }) => {
+                warn!("Invalid next message is endpoint: {endpoint:?}");
+                break;
+            }
             Ok(PortForwardDataRequest {
                 kind: Some(port_forward_data_request::Kind::Data(bytes)),
-            }) => match write_half.write_all(&bytes).await {
-                Ok(()) => (),
-                Err(error) => return warn!("Failed to write: {error}"),
+            }) => match sink.feed(bytes).await {
+                Ok(()) => {}
+                Err(error) => {
+                    warn!("Failed to write: {error}");
+                    return;
+                }
             },
-            Ok(PortForwardDataRequest { kind: None }) => return warn!("Next message is 'None'"),
-            Err(error) => return warn!("Failed to get next message: {error}"),
+            Ok(PortForwardDataRequest { kind: None }) => {
+                warn!("Next message is 'None'");
+                break;
+            }
+            Err(error) => {
+                warn!("Failed to get next message: {error}");
+                break;
+            }
         }
+    }
+    match sink.flush().await {
+        Ok(()) => {}
+        Err(error) => return warn!("Failed to flush: {error}"),
+    }
+}
+
+#[pin_project]
+struct WriteHalf(#[pin] OwnedWriteHalf);
+
+impl futures::AsyncWrite for WriteHalf {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        self.project().0.poll_write(cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        self.project().0.poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        self.project().0.poll_shutdown(cx)
+    }
+
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[std::io::IoSlice<'_>],
+    ) -> Poll<std::io::Result<usize>> {
+        self.project().0.poll_write_vectored(cx, bufs)
     }
 }
 
 #[nameth]
 #[derive(thiserror::Error, Debug)]
 pub enum UploadLocalError {
-    #[error("[{n}] No streams registered under {0:?}", n = self.name())]
-    StreamsNotRegistered(EndpointId),
-
-    #[error("[{n}] Failed to get streams: {0}", n = self.name())]
-    StreamsNotAvailable(oneshot::error::RecvError),
-
-    #[error("[{n}] No more streams", n = self.name())]
-    NoMoreStreams,
+    #[error("[{n}] Failed to connect: {0}", n = self.name())]
+    Connect(std::io::Error),
 }
 
 impl From<UploadLocalError> for Status {
     fn from(error: UploadLocalError) -> Self {
         let code = match error {
-            UploadLocalError::StreamsNotRegistered { .. } => tonic::Code::InvalidArgument,
-            UploadLocalError::StreamsNotAvailable { .. } => tonic::Code::FailedPrecondition,
-            UploadLocalError::NoMoreStreams { .. } => tonic::Code::FailedPrecondition,
+            UploadLocalError::Connect { .. } => tonic::Code::Aborted,
         };
         Self::new(code, error.to_string())
     }
