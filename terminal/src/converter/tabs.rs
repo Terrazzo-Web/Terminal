b877fa26f6a55e7b87536ac731deb8b496938f16
Lamparo <LamparoS@Pavy.one>
2025-12-20 14:07:53 +0100

    copy

--- terminal/src/converter/tabs.rs
+++ terminal/src/converter/tabs.rs
@@ -2,17 +2,31 @@
 
 use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Duration;
 
 use terrazzo::autoclone;
 use terrazzo::html;
+use terrazzo::prelude::diagnostics::warn;
 use terrazzo::prelude::*;
+use terrazzo::template;
 use terrazzo::widgets::tabs::TabDescriptor;
 use terrazzo::widgets::tabs::TabsDescriptor;
 use terrazzo::widgets::tabs::TabsState;
+use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::ClipboardEvent;
+use web_sys::DragEvent;
+use web_sys::KeyboardEvent;
+use web_sys::MouseEvent;
 
 use super::api::Conversion;
 use super::api::Conversions;
+use crate::assets::icons;
 use crate::converter::api::Language;
+use crate::frontend::utils::SleepError;
+use crate::frontend::utils::sleep;
+
+stylance::import_style!(style, "tabs.scss");
 
 impl TabsDescriptor for Conversions {
     type State = ConversionsState;
@@ -21,11 +35,80 @@ impl TabsDescriptor for Conversions {
     fn tab_descriptors(&self) -> &[Self::TabDescriptor] {
         &self.conversions
     }
+
+    fn after_titles(&self, state: &Self::State) -> impl IntoIterator<Item = impl Into<XNode>> {
+        Some(copy(state))
+    }
+}
+
+#[html]
+pub fn copy(state: &ConversionsState) -> XElement {
+    let label: XSignal<Label> = XSignal::new("copy-label", Label::default());
+    copy_impl(label, state.selected.clone())
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+enum Label {
+    #[default]
+    Ready,
+    Copying,
+    Copied,
+    Failed,
+}
+
+impl Label {
+    #[html]
+    #[template(tag = div)]
+    fn show(#[signal] this: Label) -> XElement {
+        match this {
+            Label::Ready => div(img(class = style::copy_icon, src = icons::copy()), "Copy"),
+            Label::Copying => div("Copying"),
+            Label::Copied => div(img(class = style::copy_icon, src = icons::done()), "Copied"),
+            Label::Failed => div("Failed"),
+        }
+    }
+}
+
+#[autoclone]
+#[html]
+#[template(tag = div)]
+fn copy_impl(label: XSignal<Label>, #[signal] selected: Option<Conversion>) -> XElement {
+    let Some(selected) = selected else {
+        return tag(style::visibility = "hidden", style::display = "none");
+    };
+    tag(
+        Label::show(label.clone()),
+        class = style::label,
+        click = move |_ev: MouseEvent| {
+            let window = web_sys::window().or_throw("window");
+            let clipboard = window.navigator().clipboard();
+            let promise = clipboard.write_text(&selected.content);
+            let future = JsFuture::from(promise);
+            spawn_local(async move {
+                autoclone!(label);
+                label.set(Label::Copying);
+                match future.await {
+                    Ok(ok) => {
+                        label.set(Label::Copied);
+                        diagnostics::info!("Copied into clipboard {ok:?}");
+                    }
+                    Err(err) => {
+                        label.set(Label::Failed);
+                        warn!("Failed to clipboard copy {err:?}");
+                    }
+                }
+                spawn_local(async move {
+                    let sleep: Result<(), SleepError> = sleep(Duration::from_millis(700)).await;
+                    sleep.or_else_throw(|error| format!("Failed to sleep: {error:?}"));
+                    label.set(Label::default())
+                });
+            });
+        },
+    )
 }
 
 #[derive(Clone)]
 pub struct ConversionsState {
-    #[expect(unused)]
     selected: XSignal<Option<Conversion>>,
     selected_tabs: Arc<HashMap<Language, XSignal<bool>>>,
 }
@@ -104,7 +187,53 @@ impl TabDescriptor for Conversion {
     #[html]
     fn item(&self, _state: &Self::State) -> impl Into<XNode> {
         let content = &self.content;
-        textarea("{content}")
+        pre(
+            "{content}",
+            contenteditable = "true",
+            keydown = |ev: KeyboardEvent| {
+                struct KeyEvent<'t> {
+                    alt_key: bool,
+                    shift_key: bool,
+                    ctrl_key: bool,
+                    meta_key: bool,
+                    key: &'t str,
+                }
+                match (KeyEvent {
+                    alt_key: ev.alt_key(),
+                    shift_key: ev.shift_key(),
+                    ctrl_key: ev.ctrl_key(),
+                    meta_key: ev.meta_key(),
+                    key: &ev.key(),
+                }) {
+                    KeyEvent {
+                        alt_key: false,
+                        shift_key: false,
+                        ctrl_key: true,
+                        meta_key: false,
+                        key: "c" | "a",
+                    }
+                    | KeyEvent {
+                        alt_key: false,
+                        shift_key: false,
+                        ctrl_key: false,
+                        meta_key: true,
+                        key: "c" | "a",
+                    }
+                    | KeyEvent {
+                        key: "ArrowDown" | "ArrowUp" | "ArrowLeft" | "ArrowRight",
+                        ..
+                    }
+                    | KeyEvent {
+                        key: "Home" | "End" | "PageUp" | "PageDown" | "Tab",
+                        ..
+                    } => {}
+                    KeyEvent { .. } => ev.prevent_default(),
+                }
+            },
+            drop = |ev: DragEvent| ev.prevent_default(),
+            cut = |ev: ClipboardEvent| ev.prevent_default(),
+            paste = |ev: ClipboardEvent| ev.prevent_default(),
+        )
     }
 
     fn selected(&self, state: &Self::State) -> XSignal<bool> {
