957b548f4853682f2a97e1b08d29873ea4ed009c
Lamparo <LamparoS@Pavy.one>
2025-12-21 22:38:02 +0100

    .

--- terminal/tests/run.sh
+++ terminal/tests/run.sh
@@ -5,6 +5,6 @@ cargo run --locked \
     --bin terrazzo-terminal \
     --no-default-features \
     --features server,max-level-debug,debug,diagnostics \
-    --features converter-server \
+    --features prod \
     -- \
     $@
