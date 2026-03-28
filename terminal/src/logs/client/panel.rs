30922e0e62adbc6242c7579ad668e200a8b9a55c
Richard <richard@pavy.one>
2026-03-28 11:46:37 +0000

    wip after_logs_render

--- terminal/src/logs/client/panel.rs
+++ terminal/src/logs/client/panel.rs
@@ -1,18 +1,22 @@
+use std::cell::Cell;
 use std::collections::VecDeque;
 use std::sync::Arc;
 use std::sync::LazyLock;
+use std::time::Duration;
 
 use self::diagnostics::warn;
 use terrazzo::autoclone;
 use terrazzo::html;
 use terrazzo::prelude::*;
 use terrazzo::template;
+use terrazzo::widgets::debounce::DoDebounce as _;
+use terrazzo::widgets::element_capture::ElementCapture;
+use web_sys::HtmlDivElement;
 
 use super::engine::ClientLogEvent;
 use super::engine::LogsEngine;
 use crate::frontend::mousemove::MousemoveManager;
 use crate::frontend::mousemove::Position;
-use wasm_bindgen::JsCast;
 
 stylance::import_style!(style, "panel.scss");
 
@@ -32,26 +36,40 @@ fn show_logs(#[signal] show_logs_panel: bool) -> XElement {
     if show_logs_panel {
         let logs_engine = LogsEngine::new();
         let logs = logs_engine.logs();
+        let panel = ElementCapture::<HtmlDivElement>::default();
+        let first_render = Cell::new(true).into();
         tag(
             class = style::logs_panel,
-            before_render = move |_| {
+            before_render = panel.capture(),
+            after_render = move |_| {
                 let _ = &logs_engine;
             },
-            after_render = after_logs_render,
             style::height %= logs_panel_height(RESIZE_MANAGER.delta.clone()),
             style::max_height %= logs_panel_height(RESIZE_MANAGER.delta.clone()),
-            show_logs_list(logs),
+            show_logs_list(panel.clone(), first_render, logs),
         )
     } else {
         tag(style::display = "none")
     }
 }
 
+#[autoclone]
 #[html]
 #[template(tag = ol)]
-fn show_logs_list(#[signal] logs: Arc<VecDeque<ClientLogEvent>>) -> XElement {
+fn show_logs_list(
+    panel: ElementCapture<HtmlDivElement>,
+    first_render: Ptr<Cell<bool>>,
+    #[signal] logs: Arc<VecDeque<ClientLogEvent>>,
+) -> XElement {
+    let after_render = Duration::from_millis(10).debounce(move |_| {
+        panel.with(move |panel| {
+            autoclone!(first_render);
+            after_logs_render(&first_render, panel)
+        })
+    });
     tag(
         class = style::logs_list,
+        after_render = move |e| after_render(e.clone()),
         logs.iter().map(|log| {
             let level = &log.level;
             let message = &log.message;
@@ -65,28 +83,36 @@ fn show_logs_list(#[signal] logs: Arc<VecDeque<ClientLogEvent>>) -> XElement {
     )
 }
 
-fn after_logs_render(element: &web_sys::Element) {
+fn after_logs_render(first_render: &Cell<bool>, panel: &web_sys::HtmlDivElement) {
     const DEFAULT_LINE_HEIGHT: i32 = 20;
-    let element: &web_sys::HtmlElement = element.dyn_ref().or_throw("logs panel");
-    let scroll_top = element.scroll_top();
-    let client_height = element.client_height();
-    let scroll_height = element.scroll_height();
-    let gap = scroll_height - (scroll_top + client_height);
+    let scroll_top = panel.scroll_top();
+    let client_height = panel.client_height();
+    let scroll_height = panel.scroll_height();
+
+    if first_render.replace(false) {
+        warn!(
+            "[after_logs_render] FIRST RENDER panel.set_scroll_top({scroll_height} - {client_height})"
+        );
+        panel.set_scroll_top(scroll_height - client_height);
+        return;
+    }
+
+    let gap = scroll_height - client_height - scroll_top;
 
     // Keep live-tail behavior only when user is near bottom (1-2 lines). If user has scrolled up, preserve position.
-    let li = element.query_selector("li.log-item").ok().flatten();
-    let line_height = li
-        .and_then(|li| {
-            li.dyn_ref::<web_sys::HtmlElement>()
-                .map(|li| li.client_height())
-        })
-        .unwrap_or_else(|| {
-            warn!("Failed to get log item height, defaulting to {DEFAULT_LINE_HEIGHT}px");
-            DEFAULT_LINE_HEIGHT
-        });
+    let li = panel.query_selector("ol > li").ok().flatten();
+    let line_height = li.map(|li| li.client_height()).unwrap_or_else(|| {
+        warn!("[after_logs_render] Failed to get log item height, defaulting to {DEFAULT_LINE_HEIGHT}px");
+        DEFAULT_LINE_HEIGHT
+    });
+
+    warn!("[after_logs_render] line height: {line_height}px");
 
     if gap <= line_height * 2 {
-        element.set_scroll_top(element.scroll_height());
+        panel.set_scroll_top(scroll_height - client_height);
+        warn!(
+            "[after_logs_render] DUE TO gap:{gap} <= {line_height} * 2 WE DO panel.set_scroll_top({scroll_height} - {client_height})"
+        );
     }
 }
 
