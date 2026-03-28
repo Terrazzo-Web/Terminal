dc5279b40e1e5ceec4b9d20672a39316ace4d7bc
Richard <richard@pavy.one>
2026-03-28 20:12:37 +0000

    checks

--- all.sh
+++ all.sh
@@ -6,10 +6,10 @@ cargo check --locked --no-default-features --features max-level-debug,diagnostic
 cargo check --locked --bin terrazzo-terminal --no-default-features --features server-all --features max-level-debug,diagnostics
 cargo check --locked --bin terrazzo-terminal --no-default-features --features server-all --features max-level-info
 cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features terminal-server
+cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features terminal-server,logs-panel-server
 cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features text-editor-server
 cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features converter-server
 cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features port-forward-server
-cargo check --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features logs-panel-server
 cargo check --locked --bin game --no-default-features --features server,max-level-debug,debug,diagnostics
 cargo check --locked --bin game --no-default-features --features server,max-level-info --release
 
@@ -17,10 +17,10 @@ cargo clippy --locked --no-default-features --features max-level-debug,diagnosti
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features server-all --features max-level-debug,diagnostics
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features server-all --features max-level-info
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features terminal-server
+cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features terminal-server,logs-panel-server
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features text-editor-server
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features converter-server
 cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features port-forward-server
-cargo clippy --locked --bin terrazzo-terminal --no-default-features --features max-level-debug,debug,diagnostics --features logs-panel-server
 cargo clippy --locked --bin game --no-default-features --features server,max-level-debug,debug,diagnostics
 cargo clippy --locked --bin game --no-default-features --features server,max-level-info --release
 
